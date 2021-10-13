8e811cbdb8e7bbe62efc17397b4ca10fc757dfe5
butcherfirewaters <firewaters@mail.ru>
2021-10-13 08:27:07 +0300

    finished project test 1

--- index..php
+++ index..php
@@ -1,2 +1,15 @@
 <?php
-echo '111';
+$name = 'Aleksey';
+echo $name . "\n";
+$age = 32;
+echo $age . "\n";
+$pi = pi();
+echo $pi . "\n";
+$arr1 = ['alex', 'vova', 'tolya'];
+print_r($arr1);
+$arr2 = ['alex', 'vova', 'tolya', ['kostya', 'olya']];
+print_r($arr2);
+$arr3 = ['alex', 'vova', 'tolya', ['kostya', 'olya', ['gosha', 'mila']]];
+print_r($arr3);
+$arr4 = [['Alex', 'Vova', 'Tolya'], ['Kostya', 'Olya'], ['Gosha', 'Mila']];
+print_r($arr4);
